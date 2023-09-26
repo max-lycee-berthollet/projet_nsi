1f102709982523c13e629c030d72e036925b9bb9
Ellanodem <146076419+Ellanodem@users.noreply.github.com>
2023-09-26 11:43:20 +0200

    Update projet_nsi.py

--- projet_nsi.py
+++ projet_nsi.py
@@ -3,8 +3,8 @@ while debut != "oui":
     debut=input(f"voulez vous debuter le jeu ? oui/non ")
 intro=("""\nBienvenu dans le jeu du labyrinthe, les règles sont simples:
 -des bonus sont disposés dans le labyrinthe, à vous de choisir de les rammaser ou non, ils peuvent vous aider ou pas.
--vous ne pouvez rammaser que 5 bonus par parties.
--vous devez sortir du labyrinthe avant la fin du temps impartie sous peine de ne plus  jamais en resortir.
+-vous devez rammaser entre 2 et 5 bonus par parties.
+-vous devez sortir du labyrinthe avant la fin du temps imparti sous peine de ne plus  jamais en resortir.
 Et voila vous connaissez les bases du jeu, à vous de prendre les bonnes descisions.
 """)
 print(intro)
